0381c2a4fa9dbd79a0f7d8c3e12022b2af7448f0
Megan Thomas <21thomam1@elmbrookstudents.org>
2018-09-13 13:55:33 -0500

    initials program

---
+++ src/outputexamples/Initials.java
@@ -0,0 +1,15 @@
+package outputexamples;
+
+public class Initials {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("MMMMMMMMMMM     I   TTTTTTTTT");
+		System.out.println("M    M    M     I       T");
+		System.out.println("M    M    M     I       T");
+		System.out.println("M         M     I       T");
+		System.out.println("M         M     I       T");
+	}
+
+}

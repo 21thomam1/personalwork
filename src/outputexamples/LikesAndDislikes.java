2463c62bd7a03e605b998cd4aa87211a4fb4866a
Megan Thomas <21thomam1@elmbrookstudents.org>
2018-09-13 13:43:19 -0500

    likes and dislikes program

---
+++ src/outputexamples/LikesAndDislikes.java
@@ -0,0 +1,22 @@
+package outputexamples;
+
+public class LikesAndDislikes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Likes					Dislikes");
+		
+		System.out.println("Dogs					Cats");
+		
+		System.out.println("Track					Soccer");
+		
+		System.out.println("Mac and Cheese				Watermelon");
+		
+		System.out.println("Vines					Instagram Stories");
+		
+		System.out.println("Driving					Behind the Wheel Lessons");
+		
+	}
+	
+}

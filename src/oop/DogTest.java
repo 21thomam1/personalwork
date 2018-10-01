82b72e92f79e7150e4c9bc4ab41035829f23799c
Megan Thomas <21thomam1@elmbrookstudents.org>
2018-10-01 14:54:59 -0500

    dog test program

---
+++ src/oop/DogTest.java
@@ -0,0 +1,13 @@
+package oop;
+
+public class DogTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Dog reggie = new Dog();
+		reggie.woof();
+		reggie.attack();
+	}
+
+}

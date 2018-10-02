beafdf2b2612995431ad26f0bf71f829cfb5ea11
Megan Thomas <21thomam1@elmbrookstudents.org>
2018-10-02 14:20:34 -0500

    neighbor program

---
+++ src/oop/Neighbor.java
@@ -0,0 +1,20 @@
+package oop;
+
+public class Neighbor {
+
+	public Neighbor()
+	{}
+	
+	public void sayJoke()
+	{
+		System.out.println("Want to hear my best Dad joke? It goes like this: \nI'm hungry\nNice to meet you Hungry, I'm Dad.");
+	}
+	public void sayMoney()
+	{
+		System.out.println("You want my money, Karen?");
+	}
+	public void sayConversation()
+	{
+		System.out.println("We should talk more often, I'll have you over for my wife's famous casserole. Oh, and her potato salad, too!");
+	}
+}
---
+++ src/oop/TestNeighbor.java
@@ -0,0 +1,16 @@
+package oop;
+
+public class TestNeighbor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Neighbor scot = new Neighbor();
+		Friend karen = new Friend();
+		scot.sayMoney();
+		karen.sayMessage();
+		scot.sayConversation();
+		scot.sayJoke();
+	}
+
+}

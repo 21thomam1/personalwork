3a8fa028d41f467f3aa61e311fa44dc61357aebb
Megan Thomas <21thomam1@elmbrookstudents.org>
2018-10-02 14:20:11 -0500

    car program

--- src/oop/Car.java
+++ src/oop/Car.java
@@ -1,9 +1,22 @@
 package oop;
-
+import java.util.Scanner;
 public class Car {
-
+Scanner input = new Scanner(System.in);
 	public Car()
+	{}
+		public void sayOdometer()
 	{
-		
+			int miles = -999;
+			System.out.println("How many miles are on the car?");
+			miles = input.nextInt();
+			System.out.println("There are " + miles + " miles on the car.");
 	}
+		public void sayFlat()
+	{
+			System.out.println("There is a flat tire.");
+	}
+		public void sayGas()
+		{
+			System.out.println("The car is out of gas.");
+		}
 }
---
+++ src/oop/TestCar.java
@@ -0,0 +1,14 @@
+package oop;
+
+public class TestCar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Car mator = new Car();
+		mator.sayFlat();
+		mator.sayGas();
+		mator.sayOdometer();
+	}
+
+}

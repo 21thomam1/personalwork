269d54d6b85297ab2799351fdefe89245c287b13
Megan Thomas <21thomam1@elmbrookstudents.org>
2018-10-03 14:35:23 -0500

    edited neighbor program

--- src/oop/Neighbor.java
+++ src/oop/Neighbor.java
@@ -7,7 +7,7 @@ public class Neighbor {
 	
 	public void sayJoke()
 	{
-		System.out.println("Want to hear my best Dad joke? It goes like this: \nI'm hungry\nNice to meet you Hungry, I'm Dad.");
+		System.out.println("Want to hear my best Dad joke? It goes like this: \nI'm hungry\nHi Hungry, I'm Dad.");
 	}
 	public void sayMoney()
 	{
@@ -15,6 +15,6 @@ public class Neighbor {
 	}
 	public void sayConversation()
 	{
-		System.out.println("We should talk more often, I'll have you over for my wife's famous casserole. Oh, and her potato salad, too!");
+		System.out.println("We should talk more often.\nI'll have you over for my wife's famous casserole.\nOh, and her potato salad, too!");
 	}
 }

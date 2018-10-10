4c3999cb006aa99cef2229e808d12450a95c4eaf
Megan Thomas <21thomam1@elmbrookstudents.org>
2018-10-10 15:05:10 -0500

    edited test rectangle program

--- src/oop/TestRectangle.java
+++ src/oop/TestRectangle.java
@@ -2,8 +2,26 @@ package oop;
 
 public class TestRectangle {
 
-Rectangle box = new Rectangle();
-System.out.println("box length: " + box.getLength());
-
-
+	public static void main(String[] args) {
+	Rectangle box = new Rectangle();
+	System.out.println("box length: " + box.getLength());
+	System.out.println("box width: " + box.getWidth());
+	System.out.println("box perimeter: " + box.perimeter());
+	System.out.println("box area: " + box.area());
+	box.setLength(4.5);
+	box.setWidth(7);
+	System.out.println("box length: " + box.getLength());
+	System.out.println("box width: " + box.getWidth());
+	
+	box = new Rectangle(3.0, 8.75);
+	System.out.println("box length: " + box.getLength());
+	System.out.println("box width: " + box.getWidth());
+	System.out.println("box perimeter: " + box.perimeter());
+	System.out.println("box area: " + box.area());
+	box.setLength(4.5);
+	box.setWidth(7);
+	System.out.println("box length: " + box.getLength());
+	System.out.println("box width: " + box.getWidth());
+	
+	}
 }

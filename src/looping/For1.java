3a6640d405db7429c751c02bca9890d2210e9ead
Megan <21thomam1@elmbrookstudents.org>
2018-10-25 13:41:31 -0500

    counter program

---
+++ src/looping/For1.java
@@ -0,0 +1,14 @@
+package looping;
+
+public class For1 {
+
+	public static void main(String[] args) {
+		int counter = 0;
+		
+		for(counter = 1; counter <= 5; counter = counter + 1)	{
+			System.out.println(counter);
+		}
+
+	}
+
+}

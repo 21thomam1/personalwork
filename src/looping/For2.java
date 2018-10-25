da0e1ee8fed4c97790db96a076277afb0c2c482e
Megan <21thomam1@elmbrookstudents.org>
2018-10-25 13:52:52 -0500

    for programs

--- src/looping/For2.java
+++ src/looping/For2.java
@@ -1,11 +1,10 @@
 package looping;
 
-public class For1 {
+public class For2 {
 
 	public static void main(String[] args) {
-		int counter = 0;
 		
-		for(counter = 1; counter <= 5; counter = counter + 1)	{
+		for(int counter = 1; counter <= 5; counter = counter + 1)	{
 			System.out.println(counter);
 		}
 

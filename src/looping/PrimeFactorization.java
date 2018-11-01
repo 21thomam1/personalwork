9db53a2155cbd13cab95284f00db16d7b10aab19
Megan <21thomam1@elmbrookstudents.org>
2018-11-01 14:00:28 -0500

    even more nested loop programs

--- src/looping/PrimeFactorization.java
+++ src/looping/PrimeFactorization.java
@@ -1,9 +1,13 @@
 package looping;
+import java.util.Scanner;
 
 public class PrimeFactorization {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		int counter = 0;
+		int num = 0;
+		System.out.println("Enter a ");
 
 	}
 

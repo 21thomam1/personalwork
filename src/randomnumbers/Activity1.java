82e8f9fa2172acce85d942b4d5b8989195352f33
Megan Thomas <21thomam1@elmbrookstudents.org>
2018-11-05 14:43:32 -0600

    in-class programs

---
+++ src/randomnumbers/Activity1.java
@@ -0,0 +1,23 @@
+package randomnumbers;
+import java.util.Random;
+
+public class Activity1 {
+
+	public static void main(String[] args) {
+		Random generator = new Random();
+		int min = 0;
+		int max = 1;
+		int flip = 0;
+		int heads = 0;
+		
+		for (int counter = 1; counter <= 100000; counter ++){
+			int randomNum = min + generator.nextInt(max - min + 1);
+			flip ++;
+			if (randomNum == 0)	
+				heads ++;
+		}
+		System.out.println("heads: " + heads);
+		System.out.println("tails: " + (100000 - heads));
+	}
+}
+

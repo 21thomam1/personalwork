f158e017c363d1af1b61240d605382105071cc9b
Megan Thomas <21thomam1@elmbrookstudents.org>
2018-11-12 14:44:26 -0600

    actual activity 1 program

---
+++ src/strings/Activityy1.java
@@ -0,0 +1,16 @@
+package strings;
+import java.util.Scanner;
+public class Activityy1 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("enter a sentence:");
+		String sentence = input.nextLine();
+		System.out.println(sentence.charAt(0));
+		System.out.println(sentence.charAt(2));
+		System.out.println(sentence);
+		
+
+	}
+
+}

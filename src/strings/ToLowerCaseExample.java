8fdb43e80442fa5512348010db2d1016058f2ced
Megan <21thomam1@elmbrookstudents.org>
2018-11-13 15:05:06 -0600

    edited to lowercase program

--- src/strings/ToLowerCaseExample.java
+++ src/strings/ToLowerCaseExample.java
@@ -7,7 +7,7 @@ public class ToLowerCaseExample {
 		System.out.println("enter a sentece:");
 		String sentence = input.nextLine();
 		System.out.println("the sentence in all lowercase letters is: " + sentence.toLowerCase());
-
+		System.out.println("the original sentence is: " + sentence);
 	}
 
 }

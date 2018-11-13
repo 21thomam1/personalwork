2f29498592677c29b3f99a21a42f3e3ed3277bf1
Megan <21thomam1@elmbrookstudents.org>
2018-11-13 14:43:04 -0600

    length of sentence program

---
+++ src/strings/Activity6.java
@@ -0,0 +1,16 @@
+package strings;
+import java.util.Scanner;
+public class Activity6 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("enter a sentece:");
+		String sentence = input.nextLine();
+		System.out.println("the length of the sentece is: " + sentence.length());
+		
+	
+		
+
+	}
+
+}

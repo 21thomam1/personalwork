bd476be95de91a8ec023ed21fc56c5fa65e8150f
Megan <21thomam1@elmbrookstudents.org>
2018-11-27 15:03:02 -0600

    edited friends first names program

--- src/arrays/FriendsFirstNames.java
+++ src/arrays/FriendsFirstNames.java
@@ -1,9 +1,32 @@
 package arrays;
 
+import java.util.Scanner;
+
 public class FriendsFirstNames {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		String names[] = new String[5];
+
+		for (int i = 0; i < names.length; i++) {
+			System.out.println("enter a name:");
+			names[i] = input.nextLine();
+		}
+
+		System.out.println("enter first name length:");
+		int firstNameLength = input.nextInt();
+		
+		for (int i = 0; i < names.length; i++) {
+			int firstName = names[i].indexOf(" ");
+			if(firstNameLength == firstName)	{
+				System.out.println(names[i]);
+			}
+
+
+		}
+		
+
+	
 
 	}
 

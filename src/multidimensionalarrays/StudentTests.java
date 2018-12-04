53920b2a8737d730778cc16c2fd7c2537aca12c1
Megan <21thomam1@elmbrookstudents.org>
2018-12-04 15:07:18 -0600

    student tests program

---
+++ src/multidimensionalarrays/StudentTests.java
@@ -0,0 +1,12 @@
+package multidimensionalarrays;
+
+import java.util.Scanner;
+
+public class StudentTests {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+
+	}
+
+}

c85a7e100b0fdce667fa7bbe677787c048b16435
Megan <21thomam1@elmbrookstudents.org>
2018-12-06 12:52:25 -0600

    seating chart program

--- src/multidimensionalarrays/SeatingChart.java
+++ src/multidimensionalarrays/SeatingChart.java
@@ -1,9 +1,21 @@
 package multidimensionalarrays;
+import java.util.Scanner;
 
 public class SeatingChart {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		System.out.println("number of students (must be multiple of 4)");
+		int students = input.nextInt();
+		String[][] seatingChart = new String[students][4];
+		
+		for(int row = 0; row < seatingChart.length; row++)	{
+			for(int column = 0; column < seatingChart[0].length; column++)	{
+				System.out.println("name of student:");
+				seatingChart[row][column] = input.nextLine();
+				
+			}
+		}
 
 	}
 

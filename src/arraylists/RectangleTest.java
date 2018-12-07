df13c343115fa274823068c2817c186ab6d866df
Megan <21thomam1@elmbrookstudents.org>
2018-12-07 14:56:45 -0600

    edited rectangle test program

--- src/arraylists/RectangleTest.java
+++ src/arraylists/RectangleTest.java
@@ -13,14 +13,16 @@ public class RectangleTest {
 			boxes.add(new Rectangle (50 + generator.nextInt(20), 50 + generator.nextInt(20)));
 		}
 		
-		Rectangle removed = boxes.remove(generator.nextInt(400));
-
+		Rectangle removed = boxes.remove(generator.nextInt(boxes.size()));
+		Rectangle last = boxes.remove(boxes.size()-1);
+		boxes.set(0, last);
+		
 		for (int i = boxes.size()-1; i > 0; i--) {
 			Rectangle oneBox = boxes.get(i);
 			System.out.println(oneBox.area());
 		}
 		System.out.println();
-		System.out.println("area of removed rectangle: " + removed.area());
+		System.out.println("length of removed rectangle: " + removed.getLength());
 		
 	}
 
